a66d0175be90132767d1cd9960cae8375e46c1a9
Deepak Patel <97304706+foxcode11@users.noreply.github.com>
2022-12-09 14:24:51 +0530

    server connected with API

--- app.js
+++ app.js
@@ -9,6 +9,28 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
 
+app.post("/",function(req,res){
+
+    const city=req.body.cityName
+
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=9e8b0311e676abb752e64e06f521c0d8&units=metric";
+
+    https.get(url,function(response){
+    console.log(response.statusCode);
+
+    response.on("data",function(data){
+        const weatherData= JSON.parse(data)
+        const temp= weatherData.main.temp
+        const weatherDescription=weatherData.weather[0].description
+        const icon=weatherData.weather[0].icon
+        const imgurl="http://openweathermap.org/img/wn/"+ icon+"@2x.png"
+        res.write("<p>The weather is currently"+weatherDescription+"</p>");
+        res.write("<h1>The temperature in "+city+" is "+temp+" degree Celcius</h1>");
+        res.write("<img src="+imgurl+">");
+        res.send()
+    })
+})
+})
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000");
